afe5ad52ab308ce98e606b79d227c11403c3b29a
ce <15091642+ceidas@users.noreply.github.com>
2021-12-13 17:28:12 +0000

    Revert "notes"
    This reverts commit 59164ac2249587bc384ce16dfbc6154028946606.

--- 21.12.02 - Bootstrap/11. WineShopBootstrap/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".add").click(function(){
-        toastr.success("The product added to cart");
-    })
-
-    $(".myBox img").click(function(){
-        toastr.error("You must click the add to cart button!")
-    })
-})
